92e6e15c377ed9d86a9e314fd267b3bfbe676fdb
jkosnoff <70274595+jkosnoff@users.noreply.github.com>
2023-01-06 15:23:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='phantom_US_toolbox',
-    version='0.0.16',
+    version='0.0.17',
     author='Joshua Kosnoff',
     author_email='jkosnoff@andrew.cmu.edu',
     description='This is a toolbox for analyzing phantom tFUS experiments',
