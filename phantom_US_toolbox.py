ebb6fe7def2526e990ad61a68d6e6691597f9675
jkosnoff <70274595+jkosnoff@users.noreply.github.com>
2025-02-19 14:04:40 -0500

    Add files via upload
    added nearest-neighbor support for f0's not directly within the calibration .txt file

--- phantom_US_toolbox.py
+++ phantom_US_toolbox.py
@@ -20,8 +20,8 @@ sns.set_style("white")
 
 
 class load_US_data:
-    def __init__(self, file_name, passed_params = None, scanner=0, _print=True):
-        self.print=_print
+    def __init__(self, file_name, passed_params=None, scanner=0, _print=True):
+        self.print = _print
         # Ultrasound Parameters
         if passed_params == None:
             self.params = self._parse_file_name(file_name)
@@ -49,7 +49,7 @@ class load_US_data:
     def get_max_channel_idx(self, series):
         idx = np.argwhere(series == series.max())
         max_idx = tuple(idx[0, :-1])
-        return(max_idx)
+        return (max_idx)
 
     def _get_conversion_factor(self, scanner=0):
         flag = 1
@@ -59,16 +59,24 @@ class load_US_data:
 #         print(conv)
         while flag == 1:
             try:
-                conv = pd.read_csv(txt_file, delimiter="\t", header = None, skiprows=idx)
+                conv = pd.read_csv(txt_file, delimiter="\t",
+                                   header=None, skiprows=idx)
                 flag = 0
             except:
                 idx += 1
         # print(self.params["f0"])
         # print(conv[conv[0] == self.params["f0"] / 1000000])
         # print(conv[2][conv[0] == self.params["f0"] / 1000000])
-        conversion_factor = conv[2][conv[0] == self.params["f0"] / 1000000].values[0]
+
+        if self.params["f0"] / 1000000 not in conv[0].values:
+            nearest_value = min(conv[0].values, key=lambda x: abs(
+                x - self.params["f0"] / 1000000))
+            conversion_factor = conv[2][conv[0] == nearest_value].values[0]
+        else:
+            conversion_factor = conv[2][conv[0] ==
+                                        self.params["f0"] / 1000000].values[0]
         # print(conversion_factor)
-        return(conversion_factor)
+        return (conversion_factor)
 
     def _serpentine_unravel(self, data):
         data_4D = np.array(data).reshape(self.params["len_z"],
@@ -78,9 +86,11 @@ class load_US_data:
 
         # flip y on every other z
         data_4D[::2, :, :, :] = data_4D[::2, ::-1, :, :]
-        data_4D[:, ::2, :, :] = data_4D[:, ::2,::-1, :]  # flip x on every other y
-        data_4D[::2, :, :, :] = data_4D[::2, :,::-1, :]  # flip x on every other z
-        return(data_4D)
+        data_4D[:, ::2, :, :] = data_4D[:, ::2,
+                                        ::-1, :]  # flip x on every other y
+        data_4D[::2, :, :, :] = data_4D[::2, :,
+                                        ::-1, :]  # flip x on every other z
+        return (data_4D)
 
     def _tdms_to_numpy(self, file_name):
         if self.print == True:
@@ -91,36 +101,40 @@ class load_US_data:
             if self.print == True:
                 print("Extracting Data . . .")
             data = tdms_file.groups()[0].channels()[0][:]
-            return(data)
+            return (data)
 
         else:
             if self.print == True:
                 print("Error! Unrecognized tdms formatting.")
-            return(None)
-    def _parse_file_name(self,file_name):
+            return (None)
 
-        fid = os.path.basename(fr"{file_name}".replace('\\',os.sep))
+    def _parse_file_name(self, file_name):
+
+        fid = os.path.basename(fr"{file_name}".replace('\\', os.sep))
         params = {}
-        nums = np.array([1,2,3,4,5,6,7,8,9,0],dtype=str)
-        
+        nums = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], dtype=str)
+
         try:
             dx = float(fid.split("x")[1].split("_")[0])
-            # print("dx: ", dx) 
-            params["len_x"] = int((float(fid.split("x")[0].split("_")[-1]) / dx) + 1)
+            # print("dx: ", dx)
+            params["len_x"] = int(
+                (float(fid.split("x")[0].split("_")[-1]) / dx) + 1)
         except:
             params["len_x"] = 0
-        
+
         try:
             dy = float(fid.split("y")[1].split("_")[0])
-            # print("dy: ", dy) 
-            params["len_y"] = int((float(fid.split("y")[0].split("_")[-1]) / dy) + 1)
+            # print("dy: ", dy)
+            params["len_y"] = int(
+                (float(fid.split("y")[0].split("_")[-1]) / dy) + 1)
         except:
             params["len_y"] = 0
-        
+
         try:
             dz = float(fid.split("z")[1].split("_")[0])
-            # print("dz: ", dz)                    
-            params["len_z"] = int((float(fid.split("z")[0].split("_")[-1]) / dz) + 1)
+            # print("dz: ", dz)
+            params["len_z"] = int(
+                (float(fid.split("z")[0].split("_")[-1]) / dz) + 1)
         except:
             params["len_z"] = 0
 
@@ -128,27 +142,27 @@ class load_US_data:
             params["f0"] = float(fid.split("UFF")[1].split("_")[0]) * 1e3
         except:
             params["f0"] = 0
-        
+
         try:
             params["CPP"] = float(fid.split("CPP")[1].split("_")[0])
         except:
             params["CPP"] = 0
-            
+
         try:
             params["pulse_number"] = float(fid.split("PN")[1].split("_")[0])
         except:
             params["pulse_number"] = 0
-        
+
         try:
             params["sample_rate"] = float(fid.split("SR")[1].split("_")[0])
         except:
             params["sample_rate"] = 0
-        
+
         try:
             params["PRF"] = float(fid.split("UPRF")[1].split("_")[0])
         except:
             params["PRF"] = 0
-        
+
         try:
             input_mv = fid.split("v_")[0].split("_")[-1]
             if input_mv[-1] == "m":
@@ -158,18 +172,18 @@ class load_US_data:
             params["input_mv"] = input_mv
         except:
             params["input_mv"] = 0
-        
+
         try:
             params["series_len"] = int(float(fid.split("DL")[1].split("_")[0]))
         except:
-            params["series_len"] = 0 
-        
+            params["series_len"] = 0
+
         try:
             params["title"] = "_".join(fid.strip(".tdms").split("_")[-2:])
         except:
             params["title"] = 0
-        
-        return(params)
+
+        return (params)
 
     def calc_PII(self, pulse_average=False):
         if not pulse_average:
@@ -177,7 +191,8 @@ class load_US_data:
                 self.peak_to_peak[self.max_idx] > 1.5 * self.peak_to_peak[self.max_idx].mean())[0][0]
 
             # Calculate the pressure of one pulse
-            P_t = self.data_4D[self.max_idx][0 + offset:self.PD_samps+offset] / self.conversion_factor
+            P_t = self.data_4D[self.max_idx][0 +
+                                             offset:self.PD_samps+offset] / self.conversion_factor
 
             # Take the integral
             timepoints = np.array([i * 1/self.params["sample_rate"] for i in range(
@@ -190,24 +205,24 @@ class load_US_data:
             # the first one
             ###
             PII = None
-        return(PII)
+        return (PII)
 
     def calc_Isppa(self):
         Isppa = self.calc_PII() / (self.PD * 10000.0)
-        return(Isppa)
+        return (Isppa)
 
     def calc_Ispta(self):
         Ispta = self.calc_PII() * self.params["PRF"] / 10
-        return(Ispta)
+        return (Ispta)
 
     def calc_Pmax(self):
-        return(self.peak_to_peak.max() / self.conversion_factor)
+        return (self.peak_to_peak.max() / self.conversion_factor)
 
     def calc_MI(self):
-        return(((np.abs(self.min_V) / self.conversion_factor) / 10**(6)) / (self.params["f0"] / 10**(6))**(0.5))
+        return (((np.abs(self.min_V) / self.conversion_factor) / 10**(6)) / (self.params["f0"] / 10**(6))**(0.5))
 
     def calc_Vpp_max(self):
-        return(self.max_V)
+        return (self.max_V)
 
     def plot_XY(self, param, slice_idx="max_slice", save_figures=False):
         if param.lower() == "pressure":
@@ -228,7 +243,7 @@ class load_US_data:
             slices = np.array(range(0, self.params["len_z"]))
         elif type(slice_idx) == int:
             slices = [slice_idx]
-        
+
         for _slice in slices:
             plt.figure()
             plt.xlabel("X (mm)")
@@ -290,4 +305,4 @@ class load_US_data:
         df.loc["Vmin"] = self.min_V
         df.loc["PNP"] = np.abs(self.min_V) / self.conversion_factor
         df.loc["Focal Point (Z, Y, X)"] = [np.asarray(self.max_idx)]
-        return(df)
+        return (df)
